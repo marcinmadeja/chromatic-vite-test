2e8902e60e3a978392bb2485bc90a4fb054c5568
Marcin Madeja <madeja.m.b@gmail.com>
2022-04-09 20:46:32 +0200

    Change background

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,7 +2,7 @@ import './app.css';
 import { Button } from '../common/button/button';
 
 export const App = () => {
-  return <div className="h-screen bg-gray-300 p-20">
+  return <div className="h-screen bg-gray-400 p-20">
     <Button />
   </div>;
 }
